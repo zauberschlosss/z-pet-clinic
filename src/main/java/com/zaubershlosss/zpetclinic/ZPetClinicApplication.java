7fe77a5b830b5dede6b706affe760b6f6b086ef3
Dmitriy Gordievskiy <aetmospearric.ein@gmail.com>
2020-08-31 17:14:15 +0300

    Initial project setup

---
+++ src/main/java/com/zaubershlosss/zpetclinic/ZPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.zaubershlosss.zpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zaubershlosss/zpetclinic/ZPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaubershlosss.zpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
