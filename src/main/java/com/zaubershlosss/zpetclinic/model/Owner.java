643ddad822b74c671e5ce40d1719906994f80985
Dmitriy Gordievskiy <aetmospearric.ein@gmail.com>
2020-09-01 17:40:04 +0300

    Adding POJOs. Closes issue #2

---
+++ src/main/java/com/zaubershlosss/zpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.zaubershlosss.zpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/zaubershlosss/zpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.zaubershlosss.zpetclinic.model;
+
+public class Vet extends Person {
+}
