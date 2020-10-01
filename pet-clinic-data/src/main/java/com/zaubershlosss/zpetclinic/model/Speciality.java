dd7ec6c5d44befbcc655bafa4ca54f7b65c8ad74
Dmitriy Gordievskiy <aetmospearric.ein@gmail.com>
2020-10-01 14:24:58 +0300

    Added specialities, added to Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/com/zaubershlosss/zpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.zaubershlosss.zpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
