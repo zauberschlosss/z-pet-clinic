0978be715a2ebac3b65a652c2741c9af1e774c9a
Dmitriy Gordievskiy <aetmospearric.ein@gmail.com>
2020-10-01 14:28:55 +0300

    Added properties to Owner. Closes #28

--- pet-clinic-data/src/main/java/com/zaubershlosss/zpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/zaubershlosss/zpetclinic/model/Owner.java
@@ -4,6 +4,9 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
 
     public Set<Pet> getPets() {
